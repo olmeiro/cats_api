6ec15047546438c162ad242a3589ed2f94cd0b8b
olmeiro <olme.orozco@gmail.com>
2023-01-31 09:54:42 -0500

    feat: design project

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://api.thecatapi.com/v1/images/search?limit=3&&api_key=live_l5UFrA4tPIxU7gGVsqbu3fb8hp1oZ8TxlCmRNHldJE6diDWOggjbPd12xQGhDj8t'
+const API_URL = 'https://api.thecatapi.com/v1/images/search?limit=4&&api_key=live_l5UFrA4tPIxU7gGVsqbu3fb8hp1oZ8TxlCmRNHldJE6diDWOggjbPd12xQGhDj8t'
 
 async function reload() {
   const response = await fetch(API_URL)
@@ -9,10 +9,12 @@ async function reload() {
   const img1 = document.getElementById('img1')
   const img2 = document.getElementById('img2')
   const img3 = document.getElementById('img3')
+  const img4 = document.getElementById('img4')
 
   img1.src = data[0].url
   img2.src = data[1].url
   img3.src = data[2].url
+  img4.src = data[3].url
 }
 
 reload(); //primera img
